d91f041545c5da08909ceed5644c826749b132f7
김경곤 <kg1942@invesume.com>
2018-07-03 10:16:52 +0900

    udpate info

--- db_info.js
+++ db_info.js
@@ -1,11 +1,11 @@
 module.exports = (function () {
   return {
     local: { // localhost
-      host: '52.231.15.128',
+      host: 'localhost',
       port: '5432',
-      user: 'ivs01',
-      password: 'exitem08EXITEM)*',
-      database: 'hamonia',
+      user: 'user',
+      password: 'pw',
+      database: 'db',
       multipleStatements : true	
     },
     real: { // real server db info

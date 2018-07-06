d58e7a2f85b8edacae77117242178ea8933f7afe
한정일 <hji@invesume.com>
2018-07-06 12:59:59 +0900

    Update MS Edge 브라우저 체크
    - MS Edge 브라우저 체크하도록 수정

--- public/js/index.js
+++ public/js/index.js
@@ -18,18 +18,20 @@ function browserCheck(){
 	var agt = navigator.userAgent.toLowerCase();
 	var text = '';
 	
-	if (agt.indexOf("opr/") != -1)					text = 'Opera';
-	else if (agt.indexOf("chrome") != -1)	text = 'Chrome';
+	if (agt.indexOf("edge/") != -1)				text = 'Edge';
+	else if (agt.indexOf("trident") != -1)		text = 'Trident';
+	else if (agt.indexOf("opr/") != -1)			text = 'Opera';
+	else if (agt.indexOf("chrome") != -1)		text = 'Chrome';
 	else if (agt.indexOf("staroffice") != -1)	text = 'Star Office';
 	else if (agt.indexOf("webtv") != -1)		text = 'WebTV';
-	else if (agt.indexOf("beonex") != -1)	text = 'Beonex';
-	else if (agt.indexOf("chimera") != -1)	text = 'Chimera';
-	else if (agt.indexOf("netpositive") != -1)text = 'NetPositive';
-	else if (agt.indexOf("phoenix") != -1)	text = 'Phoenix';
+	else if (agt.indexOf("beonex") != -1)		text = 'Beonex';
+	else if (agt.indexOf("chimera") != -1)		text = 'Chimera';
+	else if (agt.indexOf("netpositive") != -1)	text = 'NetPositive';
+	else if (agt.indexOf("phoenix") != -1)		text = 'Phoenix';
 	else if (agt.indexOf("firefox") != -1)		text = 'Firefox';
 	else if (agt.indexOf("safari") != -1)		text = 'Safari';
 	else if (agt.indexOf("skipstone") != -1)	text = 'SkipStone';
-	else if (agt.indexOf("netscape") != -1)	text = 'Netscape';
+	else if (agt.indexOf("netscape") != -1)		text = 'Netscape';
 	else if (agt.indexOf("mozilla/5.0") != -1)	text = 'Mozilla';
 	
 	console.log(' ---- agt //  text : ' + agt + ' // ' + text);
